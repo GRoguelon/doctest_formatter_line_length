1b419fb544ea24b46dba006f2905a83dfa0f65cf
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2024-11-18 18:56:38 -0500

    Added a very long function

--- lib/doctest_formatter_line_length.ex
+++ lib/doctest_formatter_line_length.ex
@@ -15,4 +15,16 @@ defmodule DoctestFormatterLineLength do
   def hello do
     :world
   end
+
+  @doc """
+  Dummy functions
+
+  ## Examples
+
+      iex> my_very_long_function("hello world", "My second Argument", "My third Argument", "My fourth Argument")
+      ["My second Argument", "My third Argument", "My fourth Argument"]
+  """
+  def my_very_long_function(argument_1, argument_2, argument_3, argument_4) when is_binary(argument_1) do
+    [argument_2, argument_3, argument_4]
+  end
 end
